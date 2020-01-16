99ed66aad7c6743faab9f6cfad046e6169756759
C-Mike576 <m.calkins576@gmail.com>
2020-01-16 12:05:44 -0500

    added license and seed players

---
+++ config/initializers/seed.rb
@@ -0,0 +1,111 @@
+Player.create(:name => "Brick", :position => "Squaddie",
+     :card_front_address => "../public/masons/Brick_front.png", 
+     :card_back_address => "../public/masons/Brick_back.png")
+
+Player.create(:name => "Champ", :position => "Squaddie",
+    :card_front_address => "../public/masons/Champ_front.png", 
+    :card_back_address => "../public/masons/Champ_back.png")
+
+Player.create(:name => "Chisel", :position => "Squaddie",
+    :card_front_address => "../public/masons/Chisel_front.png", 
+    :card_back_address => "../public/masons/Chisel_back.png")
+
+Player.create(:name => "Corbelli", :position => "Captian",
+    :card_front_address => "../public/masons/Corbelli_front.png", 
+    :card_back_address => "../public/masons/Corbelli_back.png")
+
+Player.create(:name => "Flint", :position => "Squaddie",
+    :card_front_address => "../public/masons/Flint_front.png", 
+    :card_back_address => "../public/masons/Flint_back.png")
+
+Player.create(:name => "Granite", :position => "Squaddie",
+    :card_front_address => "../public/masons/Granite_front.png", 
+    :card_back_address => "../public/masons/Granite_back.png")
+
+Player.create(:name => "Hammer", :position => "Captian",
+    :card_front_address => "../public/masons/Hammer_front.png", 
+    :card_back_address => "../public/masons/Hammer_back.png")
+   
+Player.create(:name => "Harmony", :position => "Squaddie",
+    :card_front_address => "../public/masons/Harmony_front.png", 
+    :card_back_address => "../public/masons/Harmony_back.png")
+   
+Player.create(:name => "Honour", :position => "Captian",
+    :card_front_address => "../public/masons/Honour_front.png", 
+    :card_back_address => "../public/masons/Honour_back.png")
+   
+Player.create(:name => "Mallet", :position => "Squaddie",
+    :card_front_address => "../public/masons/Mallet_front.png", 
+    :card_back_address => "../public/masons/Mallet_back.png")
+   
+Player.create(:name => "Marbles", :position => "Mascot",
+    :card_front_address => "../public/masons/Marbles_front.png", 
+    :card_back_address => "../public/masons/Marbles_back.png")
+   
+Player.create(:name => "Tower", :position => "Squaddie",
+    :card_front_address => "../public/masons/Tower_front.png", 
+    :card_back_address => "../public/masons/Tower_back.png")
+   
+Player.create(:name => "Wrecker", :position => "Mascot",
+    :card_front_address => "../public/masons/Wrecker_front.png", 
+    :card_back_address => "../public/masons/Wrecker_back.png")
+   
+Player.create(:name => "Corker", :position => "Captian",
+    :card_front_address => "../public/masons/Corker_front.png", 
+    :card_back_address => "../public/masons/Corker_back.png")
+   
+Player.create(:name => "Decimate", :position => "Squaddie",
+    :card_front_address => "../public/masons/Decimate_front.png", 
+    :card_back_address => "../public/masons/Decimate_back.png")    
+        
+Player.create(:name => "Esters", :position => "Captian",
+    :card_front_address => "../public/masons/Esters_front.png", 
+    :card_back_address => "../public/masons/Esters_back.png")
+   
+Player.create(:name => "Flea", :position => "Squaddie",
+    :card_front_address => "../public/masons/Flea_front.png", 
+    :card_back_address => "../public/masons/Flea_back.png")
+   
+Player.create(:name => "Friday", :position => "Squaddie",
+    :card_front_address => "../public/masons/Friday_front.png", 
+    :card_back_address => "../public/masons/Friday_back.png")
+   
+Player.create(:name => "Hooper", :position => "Squaddie",
+    :card_front_address => "../public/masons/Hooper_front.png", 
+    :card_back_address => "../public/masons/Hooper_back.png")
+   
+Player.create(:name => "Lucky", :position => "Squaddie",
+    :card_front_address => "../public/masons/Lucky_front.png", 
+    :card_back_address => "../public/masons/Lucky_back.png")
+   
+Player.create(:name => "Mash", :position => "Squaddie",
+    :card_front_address => "../public/masons/Mash_front.png", 
+    :card_back_address => "../public/masons/Mash_back.png")
+   
+Player.create(:name => "Pintpot", :position => "Squaddie",
+    :card_front_address => "../public/masons/Pintpot_front.png", 
+    :card_back_address => "../public/masons/Pintpot_back.png")
+   
+Player.create(:name => "Quaff", :position => "Mascot",
+    :card_front_address => "../public/masons/Quaff_front.png", 
+    :card_back_address => "../public/masons/Quaff_back.png")
+   
+Player.create(:name => "Scum", :position => "Mascot",
+    :card_front_address => "../public/masons/Scum_front.png", 
+    :card_back_address => "../public/masons/Scum_back.png")
+   
+Player.create(:name => "Spigot", :position => "Squaddie",
+    :card_front_address => "../public/masons/Spigot_front.png", 
+    :card_back_address => "../public/masons/Spigot_back.png")
+   
+Player.create(:name => "Stave", :position => "Squaddie",
+    :card_front_address => "../public/masons/Stave_front.png", 
+    :card_back_address => "../public/masons/Stave_back.png")
+   
+Player.create(:name => "Stoker", :position => "Squaddie",
+    :card_front_address => "../public/masons/Stoker_front.png", 
+    :card_back_address => "../public/masons/Stoker_back.png")
+        
+Player.create(:name => "Tapper", :position => "Captian",
+    :card_front_address => "../public/masons/Tapper_front.png", 
+    :card_back_address => "../public/masons/Tapper_back.png")
