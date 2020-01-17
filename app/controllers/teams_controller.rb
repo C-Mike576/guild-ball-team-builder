a3b93d7c7d08584cc060aae65afe787ba79013c6
C-Mike576 <m.calkins576@gmail.com>
2020-01-17 16:40:24 -0500

    finish team show page final version 1.0

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,7 +2,6 @@ class TeamsController < ApplicationController
 
     get '/teams' do
         if_not_logged_in_redirect
-
         @teams = Team.where(user_id: session[:user_id])
         erb :"teams/index"
     end
@@ -34,17 +33,25 @@ class TeamsController < ApplicationController
         erb :"teams/edit"
     end
 
-    post 'teams/:id' do
+    post '/teams/:id' do
         if_not_logged_in_redirect
-        correct_user?(@team)
-        @team.update(params)
+        @team = Team.find_by(:id => params[:id])
+        @team.update(params.select{|p| p == :captian_id || p == :name ||
+        p == :mascot_id || p == :squaddie_1_id || p == :squaddie_2_id||
+         p == :squaddie_3_id || p == :squaddie_4_id})
         redirect '/teams'
     end
 
-    get 'teams/:id' do
+    get '/teams/:id' do
         if_not_logged_in_redirect
         @picked_team = Team.find(params[:id])
         correct_user?(@picked_team)
+        @captian = Player.find_by(:id => @picked_team.captian_id)
+        @mascot = Player.find_by(:id => @picked_team.mascot_id)
+        @squaddie_1 = Player.find_by(:id => @picked_team.squaddie_1_id)
+        @squaddie_2 = Player.find_by(:id => @picked_team.squaddie_2_id)
+        @squaddie_3 = Player.find_by(:id => @picked_team.squaddie_3_id)
+        @squaddie_4 = Player.find_by(:id => @picked_team.squaddie_4_id)
         erb :"teams/show"
     end
 
